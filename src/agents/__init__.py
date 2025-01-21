003351d2293db6d66e8ce8d69dce235fb0eb6128
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-21 08:19:43 -0800

    Update __init__.py

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,1 +1,21 @@
+from .central_cognitive_agent import CentralCognitiveAgent
+from .sensory_agent import SensoryAgent
+from .planning_agent import PlanningAgent
+from .manipulation_agent import ManipulationAgent
+from .motor_control_agent import MotorControlAgent
+from .energy_management_agent import EnergyManagementAgent
+from .learning_agent import LearningAgent
+from .communication_agent import CommunicationAgent
+
+# Define the public API of the `agents` package
+__all__ = [
+    "CentralCognitiveAgent",
+    "SensoryAgent",
+    "PlanningAgent",
+    "ManipulationAgent",
+    "MotorControlAgent",
+    "EnergyManagementAgent",
+    "LearningAgent",
+    "CommunicationAgent",
+]
 
