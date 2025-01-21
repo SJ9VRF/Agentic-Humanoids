d40bf7af99a0e8bcc7dda85161845f2de654d1a6
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-21 09:28:37 -0800

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,1 +1,43 @@
+import logging
+
+
+class Logger:
+    """
+    A utility class for logging messages with different severity levels.
+    """
+
+    def __init__(self, name="RobotSystemLogger"):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        console_handler.setFormatter(formatter)
+
+        # File handler
+        file_handler = logging.FileHandler(f"{name}.log")
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        # Add handlers
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+
+    def info(self, message):
+        """Log an informational message."""
+        self.logger.info(message)
+
+    def warning(self, message):
+        """Log a warning message."""
+        self.logger.warning(message)
+
+    def error(self, message):
+        """Log an error message."""
+        self.logger.error(message)
+
+    def debug(self, message):
+        """Log a debug message."""
+        self.logger.debug(message)
 
